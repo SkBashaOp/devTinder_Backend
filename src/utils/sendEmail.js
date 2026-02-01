ace3bd99f6a7b1f4b1b47dfaae49947b7f47290f
Shaik Mohammed Hussain Basha <73814469+SkBashaOp@users.noreply.github.com>
2026-02-01 16:44:38 +0530

    Update sendEmail.js

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -32,8 +32,7 @@ const createSendEmailCommand = (toAddress, fromAddress, subject, body) => {
 
 const run = async (subject, body, toEmailId) => {
   const sendEmailCommand = createSendEmailCommand(
-    "akshaysaini.in@gmail.com",
-    "akshay@devtinder.in",
+    "hussainfractions@gmail.com",
     subject,
     body
   );
